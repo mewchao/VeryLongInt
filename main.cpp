0bcd920117e19715b7e4876e73a8935010a9898f
youjunchao <2835190741@qq.com>
2023-05-25 15:22:36 +0800

    main设计结束

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ int main() {
     //一组测试数据的一个操作数为自己的18位身份证号码（最后一位如果为X用0取代），另一个操作数为自己的9位学号
     VeryLongInt verylongint1("350627200407240014");
     VeryLongInt verylongint2("222200231");
+    //任意长整数op任意长整形
     std::cout<<"verylongint1:"<<verylongint1<<"verylongint2:"<<verylongint2;
     std::cout<<"verylongint1+verylongint2:"<<verylongint1+verylongint2;
     std::cout<<"verylongint1-verylongint2:"<<verylongint1-verylongint2;
@@ -37,6 +38,21 @@ int main() {
     //分页处理
     system("pause");
     system("cls");
+
+    //任意长整数op字符串
+    std::cout<<"verylongint1+\"0x124896819ab4879498e4f8c8948aef\""<<verylongint1+"0x124896819ab4879498e4f8c8948aef";
+    std::cout<<"verylongint1-\"0x124896819ab4879498e4f8c8948aef\":"<<verylongint1-"0x124896819ab4879498e4f8c8948aef";
+    std::cout<<"verylongint1*\"0x124896819ab4879498e4f8c8948aef\":"<<verylongint1*"0x124896819ab4879498e4f8c8948aef";
+    std::cout<<"verylongint1/\"0x124896819ab4879498e4f8c8948aef\":"<<verylongint1/"0x124896819ab4879498e4f8c8948aef";
+    std::cout<<"verylongint1%\"0x124896819ab4879498e4f8c8948aef\":"<<verylongint1%"0x124896819ab4879498e4f8c8948aef";
+
+
+
+    system("pause");
+    system("cls");
+
+
+
     //一组2个操作数都正好是92位的数值
     VeryLongInt verylongint3("71628373872638493782649873264792647362482674628347283648764932746298347623487236472384627846");
     VeryLongInt verylongint4("56893472983746329834763298476392746928374692837469283469283746928347692843769283746928374628");
@@ -47,6 +63,17 @@ int main() {
     std::cout<<"verylongint3/verylongint4:"<<verylongint3/verylongint4;
     std::cout<<"verylongint3%verylongint4:"<<verylongint3%verylongint4;
     //分页处理
+    system("pause");
+    system("cls");
+    //任意长整数op长整型
+    std::cout<<"verylongint3+165465196"<<verylongint3+165465196;
+    std::cout<<"verylongint3-165465196:"<<verylongint3-165465196;
+    std::cout<<"verylongint3*165465196:"<<verylongint3*165465196;
+    std::cout<<"verylongint3/165465196:"<<verylongint3/165465196;
+    std::cout<<"verylongint3%165465196:"<<verylongint3%165465196;
+
+
+
     system("pause");
     system("cls");
     //default
@@ -71,8 +98,23 @@ int main() {
     std::cout<<"verylongint12:"<<verylongint12;
 //    verylongint12.is_negative();
     std::cout<<"verylongint13:"<<verylongint13;
+    system("pause");
+    system("cls");
+
+
+
+
+
+
+
 
     system("pause");
     system("cls");
+
+
+
+
+
+
     return 0;
 }
